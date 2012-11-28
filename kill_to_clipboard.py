165989ef66ec2df874e4f4716e33fa88e42a4c46
Makani Mason <gitauthor@pqmland.com>
2012-11-28 10:02:08 -0700

    implement yank from clipboard to complete proper kill implementation

--- kill_to_clipboard.py
+++ kill_to_clipboard.py
@@ -6,23 +6,31 @@ class KillLocation:
     def __init__(self):
         self.kill_points = []
         self.kill_id = 0
+        self.locked = False
 
-    def unchanged(self, view, regions):
-        append = True
+    def performed_kill(self, view, regions):
         view_id = view.id
         if view_id != self.kill_id:
-            append = False
+            self.locked = True
 
         compare_points = []
         for r in regions:
             compare_points.append(r.begin())
 
         if compare_points != self.kill_points:
-            append = False
+            self.locked = True
 
         self.kill_points = compare_points
         self.kill_id = view_id
-        return append
+
+    def lock(self):
+        self.locked = True
+
+    def unlock(self):
+        self.locked = False
+
+    def is_locked(self):
+        return self.locked
 
 kill_location = KillLocation()
 
@@ -38,10 +46,18 @@ class KillToClipboardCommand(sublime_plugin.TextCommand):
                 s = sublime.Region(s.a, s.a + 1)
             text_items.append(self.view.substr(s))
             regions.append(s)
-
         text = "\n".join(text_items)
-        if kill_location.unchanged(self.view, regions):
-            sublime.set_clipboard(sublime.get_clipboard() + text)
-        else:
+
+        kill_location.performed_kill(self.view, regions)
+        if kill_location.is_locked():
             sublime.set_clipboard(text)
+        else:
+            sublime.set_clipboard(sublime.get_clipboard() + text)
+        kill_location.unlock()
         self.view.run_command("right_delete")
+
+
+class YankFromClipboardCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        kill_location.lock()
+        self.view.run_command("paste")
