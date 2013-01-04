98a3e66348bb8b4eb1a4094a3ab11bc76f5efa26
Makani Mason <gitauthor@pqmland.com>
2013-01-04 10:32:13 -0700

    fix some bugs

--- root/kill_to_clipboard.py
+++ root/kill_to_clipboard.py
@@ -8,22 +8,25 @@ class KillLocation:
         self.kill_id = 0
         self.locked = False
 
-    def performed_kill(self, view, regions):
-        view_id = view.id
+    def check_for_cursor_change(self, view, save):
+        view_id = view.id()
         if view_id != self.kill_id:
-            print "kill_to_clipboard: starting kill; view id changed, setting locked to true"
+            print "kill_to_clipboard: check_for_cursor_change; view id changed, setting locked to true"
             self.locked = True
 
         compare_points = []
-        for r in regions:
-            compare_points.append(r.begin())
+        for selreg in view.sel():
+            compare_points.append(selreg.begin())
 
         if compare_points != self.kill_points:
-            print "kill_to_clipboard: starting kill; points changed, setting locked to true"
+            print "kill_to_clipboard: check_for_cursor_change; points changed, setting locked to true"
             self.locked = True
 
-        self.kill_points = compare_points
-        self.kill_id = view_id
+        if save:
+            print "kill_to_clipboard: setting kill points to " + str(self.kill_points)
+            print "kill_to_clipboard: setting kill_id to " + str(self.kill_id)
+            self.kill_points = compare_points
+            self.kill_id = view_id
 
     def lock(self):
         self.locked = True
@@ -39,19 +42,16 @@ kill_location = KillLocation()
 
 class KillToClipboardCommand(sublime_plugin.TextCommand):
     def run(self, edit):
-        self.view.run_command("move_to", {"to": "hardeol", "extend": True})
+        kill_location.check_for_cursor_change(self.view, True)
 
+        self.view.run_command("move_to", {"to": "hardeol", "extend": True})
         text_items = []
-        regions = []
         for selreg in self.view.sel():
             if selreg.empty():
                 selreg = sublime.Region(selreg.a, selreg.a + 1)
             text_items.append(self.view.substr(selreg))
-            regions.append(selreg)
         text = "\n".join(text_items)
         print "kill_to_clipboard: selected text " + str(text)
-
-        kill_location.performed_kill(self.view, regions)
 
         if kill_location.is_locked():
             print "kill_to_clipbard: is locked; replacing clipboard contents"
@@ -59,6 +59,7 @@ class KillToClipboardCommand(sublime_plugin.TextCommand):
         else:
             print "kill_to_clipbard: is NOT locked; appending to clipboard"
             sublime.set_clipboard(sublime.get_clipboard() + text)
+
         kill_location.unlock()
         print "kill_to_clipboard: done with clipboard; set to unlock"
         self.view.run_command("right_delete")
@@ -75,3 +76,8 @@ class KillToClipboardEvents(sublime_plugin.EventListener):
     def on_activated(self, view):
         print "kill_to_clipboard: locking in on_activated event"
         kill_location.lock()
+
+    def on_selection_modified(self, view):
+        print "kill_to_clipboard: inside on_selection_modified"
+        if not kill_location.is_locked():
+            kill_location.check_for_cursor_change(view, False)
