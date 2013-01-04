970a50b2f2715b1c5078b7b4f6cc5f12e1395871
Makani Mason <gitauthor@pqmland.com>
2013-01-04 09:32:33 -0700

    add debugging

--- root/kill_to_clipboard.py
+++ root/kill_to_clipboard.py
@@ -11,6 +11,7 @@ class KillLocation:
     def performed_kill(self, view, regions):
         view_id = view.id
         if view_id != self.kill_id:
+            print "kill_to_clipboard: starting kill; view id changed, setting locked to true"
             self.locked = True
 
         compare_points = []
@@ -18,6 +19,7 @@ class KillLocation:
             compare_points.append(r.begin())
 
         if compare_points != self.kill_points:
+            print "kill_to_clipboard: starting kill; points changed, setting locked to true"
             self.locked = True
 
         self.kill_points = compare_points
@@ -47,22 +49,29 @@ class KillToClipboardCommand(sublime_plugin.TextCommand):
             text_items.append(self.view.substr(selreg))
             regions.append(selreg)
         text = "\n".join(text_items)
+        print "kill_to_clipboard: selected text " + str(text)
 
         kill_location.performed_kill(self.view, regions)
+
         if kill_location.is_locked():
+            print "kill_to_clipbard: is locked; replacing clipboard contents"
             sublime.set_clipboard(text)
         else:
+            print "kill_to_clipbard: is NOT locked; appending to clipboard"
             sublime.set_clipboard(sublime.get_clipboard() + text)
         kill_location.unlock()
+        print "kill_to_clipboard: done with clipboard; set to unlock"
         self.view.run_command("right_delete")
 
 
 class YankFromClipboardCommand(sublime_plugin.TextCommand):
     def run(self, edit):
+        print "kill_to_clipboard: locking inside yank"
         kill_location.lock()
         self.view.run_command("paste")
 
 
 class KillToClipboardEvents(sublime_plugin.EventListener):
     def on_activated(self, view):
+        print "kill_to_clipboard: locking in on_activated event"
         kill_location.lock()
